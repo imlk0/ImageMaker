89b8e91b1fb91a76ccb0f03c6a063f1668773077
KB5201314 <ylmfyzbt@gmail.com>
2017-07-02 21:45:13 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,8 +16,7 @@ class main(object):
 
     def get_first_img(self):
         str = raw_input("表面图片：")
-        if str == "":
-            str = "first.jpeg"
+        
         self.fim = Image.open(str).convert('L')
         self.fim_size = self.fim.size
         
@@ -26,8 +25,7 @@ class main(object):
     
     def get_next_img(self):
         str = raw_input("隐藏图片：")
-        if str == "":
-            str = "next.jpg"
+        
         self.nim = Image.open(str).convert('L')
         self.nim_size = self.nim.size
         
@@ -45,6 +43,7 @@ class main(object):
     
     def write_new_img(self):
         for y in range(0,self.new_size[1]):#每一行
+            
             i = int((y*100.0)/self.new_size[1] )
             sys.stdout.write("[" + ("#" * int(i/2) ) + (" " * (50-int(i/2))) +"]" + str(i) + "%" +"\r")
             sys.stdout.flush()
